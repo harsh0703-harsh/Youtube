5cfa7ddec7c453347cb19d40463d98539f989434
Harsh <62800276+harsh0703-harsh@users.noreply.github.com>
2021-01-28 22:19:42 +0530

    Add files via upload

---
+++ HowToSendImages in Gmail by python/main.py
@@ -0,0 +1,42 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders
+
+from_address="harsh-cse19@satyug.edu.in"
+password="harsharoradaguku@12345"
+to_address="harsharora0703@gmail.com"
+
+
+#### Writing content of the mail
+msg = MIMEMultipart()    
+msg['From'] = from_address
+msg['To'] = to_address 
+msg['Subject'] = "3rd Tutorial on mailing System"
+body = "Sending one or more images"
+msg.attach(MIMEText(body, 'plain')) 
+
+# for attaching the images
+
+filename=["applicant-tracking-system-138k.png","dog1.jpeg","dog2.jpg"]
+for i in filename:
+    reading=open(i,"rb")
+    p = MIMEBase('application', 'octet-stream')
+    p.set_payload((reading).read())
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment",filename=i)
+    msg.attach(p)
+
+## Starting the main work
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+s.ehlo()
+s.starttls() 
+ 
+s.login(from_address, password) 
+   
+text = msg.as_string() 
+
+s.sendmail(from_address, to_address, text) 
+
+s.quit() 
