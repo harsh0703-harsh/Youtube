c9a23a1657847602ed965c9a9affc9110067485b
Harsh <62800276+harsh0703-harsh@users.noreply.github.com>
2021-02-11 22:34:09 +0530

    Update sketch.py

--- Open-cv Tutorials/sketch.py
+++ Open-cv Tutorials/sketch.py
@@ -1,17 +1,6 @@
 import cv2
 import os
-# img=cv2.imread("dog1.jpeg")
-# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# img_invert = cv2.bitwise_not(img_gray)
-# img_smoothing = cv2.GaussianBlur(img_invert, (21, 21),sigmaX=0, sigmaY=0)
 
-# def Remove(x, y):
-#     return cv2.divide(x, 255 - y, scale=256)
-#     ## 
-# final_img = Remove(img_gray, img_smoothing)
-# cv2.imshow("Sketch",final_img)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
 
 video=cv2.VideoCapture(0)
 i=0
